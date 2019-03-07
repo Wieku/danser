4e4c8e2aa532f9603bc8303b2dfc9cae864cc54e
Sebastian Krajewski <mail@wieku.me>
2019-03-07 22:48:38 +0100

    Add VertexAttribute

---
+++ src/main/kotlin/me/wieku/framework/graphics/VertexAttribute.kt
@@ -0,0 +1,22 @@
+package me.wieku.framework.graphics
+
+import org.lwjgl.opengl.GL33.*
+
+enum class VertexAttributeType(val glId: Int) {
+	GlInt(GL_INT),
+	GlFloat(GL_FLOAT),
+	Vec2(GL_FLOAT_VEC2),
+	Vec3(GL_FLOAT_VEC3),
+	Vec4(GL_FLOAT_VEC4),
+	Mat2(GL_FLOAT_MAT2),
+	Mat23(GL_FLOAT_MAT2x3),
+	Mat24(GL_FLOAT_MAT2x4),
+	Mat3(GL_FLOAT_MAT3),
+	Mat32(GL_FLOAT_MAT3x2),
+	Mat34(GL_FLOAT_MAT3x4),
+	Mat4(GL_FLOAT_MAT4),
+	Mat42(GL_FLOAT_MAT4x2),
+	Mat43(GL_FLOAT_MAT4x3),
+}
+
+data class VertexAttribute(val attributeName: String, val attributeType: VertexAttributeType, val attributeIndex: Int)
