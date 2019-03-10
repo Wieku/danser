dc54591d50c1f3bf4eba3e1258b83f7c243302f3
Sebastian Krajewski <mail@wieku.me>
2019-03-10 03:33:17 +0100

    Add size to VertexAttribute

--- src/main/kotlin/me/wieku/framework/graphics/VertexAttribute.kt
+++ src/main/kotlin/me/wieku/framework/graphics/VertexAttribute.kt
@@ -2,21 +2,21 @@ package me.wieku.framework.graphics
 
 import org.lwjgl.opengl.GL33.*
 
-enum class VertexAttributeType(val glId: Int) {
-	GlInt(GL_INT),
-	GlFloat(GL_FLOAT),
-	Vec2(GL_FLOAT_VEC2),
-	Vec3(GL_FLOAT_VEC3),
-	Vec4(GL_FLOAT_VEC4),
-	Mat2(GL_FLOAT_MAT2),
-	Mat23(GL_FLOAT_MAT2x3),
-	Mat24(GL_FLOAT_MAT2x4),
-	Mat3(GL_FLOAT_MAT3),
-	Mat32(GL_FLOAT_MAT3x2),
-	Mat34(GL_FLOAT_MAT3x4),
-	Mat4(GL_FLOAT_MAT4),
-	Mat42(GL_FLOAT_MAT4x2),
-	Mat43(GL_FLOAT_MAT4x3);
+enum class VertexAttributeType(val glId: Int, val size: Int) {
+	GlInt(GL_INT, 4),
+	GlFloat(GL_FLOAT, 4),
+	Vec2(GL_FLOAT_VEC2, 2 * 4),
+	Vec3(GL_FLOAT_VEC3, 3 * 4),
+	Vec4(GL_FLOAT_VEC4, 4 * 4),
+	Mat2(GL_FLOAT_MAT2, 2 * 2 * 4),
+	Mat23(GL_FLOAT_MAT2x3, 2 * 3 * 4),
+	Mat24(GL_FLOAT_MAT2x4, 2 * 4 * 4),
+	Mat3(GL_FLOAT_MAT3, 3 * 3 * 4),
+	Mat32(GL_FLOAT_MAT3x2, 3 * 2 * 4),
+	Mat34(GL_FLOAT_MAT3x4, 2 * 4 * 4),
+	Mat4(GL_FLOAT_MAT4, 4 * 4 * 4),
+	Mat42(GL_FLOAT_MAT4x2, 4 * 2 * 4),
+	Mat43(GL_FLOAT_MAT4x3, 4 * 3 * 4);
 
 	companion object {
 		private var attributeMap = HashMap<Int, VertexAttributeType>()
