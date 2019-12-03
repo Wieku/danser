32c72fbc430f9bf1f840950cb35241eb3c0e8db4
Wieku <mail@wieku.me>
2019-12-03 20:21:18 +0100

    Add possibility to bind bindables together

--- framework/src/main/kotlin/me/wieku/framework/di/bindable/Bindable.kt
+++ framework/src/main/kotlin/me/wieku/framework/di/bindable/Bindable.kt
@@ -4,14 +4,54 @@ import java.lang.ref.WeakReference
 
 class Bindable<T> (startValue: T? = null) {
 
-    var value: T? = startValue
+    /**
+     * Backing value for this bindable
+     */
+    private var _value: T? = startValue
+
+    var value: T?
+        get() = _value
         set(value) {
-            listeners.removeIf { it.get() == null }
-            listeners.forEach { it.get()?.valueChanged(this) }
-            field = value
+            if (value == _value) return
+
+            _value = value
+
+            setValue(value, this)
         }
 
     private val listeners = ArrayList<WeakReference<BindableListener<T>>>()
+
+    private val bounded = ArrayList<WeakReference<Bindable<T>>>()
+
+    private fun setValue(value: T?, source: Bindable<T>) {
+        bounded.removeIf { it.get() == null }
+
+        bounded.forEach {
+            val bindable = it.get()!!
+            if (source == bindable) return@forEach
+
+            bindable.setValue(value, this)
+        }
+
+        notifyListeners()
+    }
+
+    private fun notifyListeners() {
+        listeners.removeIf { it.get() == null }
+        listeners.forEach { it.get()?.valueChanged(this) }
+    }
+
+    fun bindTo(bindable: Bindable<T>) {
+        value = bindable._value
+
+        bounded += WeakReference(bindable)
+        bindable.bounded += WeakReference(this)
+    }
+
+    fun unbindFrom(bindable: Bindable<T>) {
+        bounded.removeIf { it.get() == null || it.get()!! == bindable }
+        bindable.bounded.removeIf { it.get() == null || it.get()!! == this }
+    }
 
     fun addListener(listener: BindableListener<T>, notifyNow: Boolean = false) {
         listeners.add(WeakReference(listener))
