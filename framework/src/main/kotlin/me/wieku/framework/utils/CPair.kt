01812a383b8c130e710a4d6adfb077e87839204b
Wieku <mail@wieku.me>
2019-12-08 09:19:20 +0100

    Add CPair that has it's own hash code

---
+++ framework/src/main/kotlin/me/wieku/framework/utils/CPair.kt
@@ -0,0 +1,23 @@
+package me.wieku.framework.utils
+
+import java.io.Serializable
+import java.util.*
+
+data class CPair<out A, out B>(
+    val first: A,
+    val second: B
+) : Serializable {
+
+    /**
+     * Returns string representation of the [Pair] including its [first] and [second] values.
+     */
+    override fun toString(): String = "($first, $second)"
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Pair<*, *>) return false
+        return first == other.first && second == other.second
+    }
+
+    override fun hashCode(): Int = Objects.hash(first, second)
+}
