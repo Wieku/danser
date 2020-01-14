3e7b4e83ec05421a98841a3774db2733db378f84
Wieku <mail@wieku.me>
2020-01-14 21:01:03 +0100

    Add CentripetalCatmullRomApproximator

---
+++ framework/src/main/kotlin/me/wieku/framework/math/curves/approximation/CentripetalCatmullRomApproximator.kt
@@ -0,0 +1,16 @@
+package me.wieku.framework.math.curves.approximation
+
+import me.wieku.framework.math.curves.CentripetalCatmullRom
+import me.wieku.framework.math.curves.Line
+
+class CentripetalCatmullRomApproximator(private val detail: Int, private val curve: CentripetalCatmullRom) : PathApproximator {
+    override fun approximate(): Array<Line> {
+
+        return Array(detail) { p ->
+            Line(
+                curve.pointAt(p.toFloat() / detail),
+                curve.pointAt((p + 1).toFloat() / detail)
+            )
+        }
+    }
+}
