d15a762037a25f25b8400cb75cbaeaa052e5860f
Wieku <mail@wieku.me>
2020-02-06 07:27:16 +0100

    Add triangles to MenuBackground

--- core/src/main/kotlin/me/wieku/danser/ui/common/background/MenuBackground.kt
+++ core/src/main/kotlin/me/wieku/danser/ui/common/background/MenuBackground.kt
@@ -1,14 +1,16 @@
 package me.wieku.danser.ui.common.background
 
 import me.wieku.danser.beatmap.Beatmap
+import me.wieku.danser.graphics.drawables.triangles.TriangleOrder
+import me.wieku.danser.graphics.drawables.triangles.Triangles
 import me.wieku.framework.animation.Transform
 import me.wieku.framework.animation.TransformType
 import me.wieku.framework.di.bindable.Bindable
-import me.wieku.framework.di.bindable.BindableListener
-import me.wieku.framework.graphics.drawables.containers.BlurredContainer
+import me.wieku.framework.graphics.drawables.containers.Container
 import me.wieku.framework.graphics.drawables.containers.ParallaxContainer
 import me.wieku.framework.graphics.drawables.sprite.Sprite
 import me.wieku.framework.graphics.drawables.sprite.SpriteBatch
+import me.wieku.framework.graphics.pixmap.Pixmap
 import me.wieku.framework.graphics.textures.Texture
 import me.wieku.framework.math.Origin
 import me.wieku.framework.math.Scaling
@@ -18,23 +20,24 @@ import org.joml.Vector2f
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 import java.io.File
+import java.util.*
 
-class MenuBackground(): ParallaxContainer(), KoinComponent {
+class MenuBackground() : ParallaxContainer(), KoinComponent {
 
     //TODO: texture unloading
 
-    val defaultBackground: Sprite
+    private val beatmapBindable: Bindable<Beatmap?> by inject()
+    private val beatmap = Bindable<Beatmap?>(null)
 
-    private lateinit var wrapper: BackgroundWrapper
+    private val random = Random()
 
-    var fileTextureHandle: FileHandle? = null
+    private val wrapper: Container
+    private val triangles: Triangles
 
-    var oldBackground: Sprite? = null
-    var currentBackground: Sprite? = null
+    private var pixmap: Pixmap? = null
 
-    val beatmapBindable: Bindable<Beatmap?> by inject()
-
-    val beatmap = Bindable<Beatmap?>(null)
+    private var oldBackground: Sprite? = null
+    private var currentBackground: Sprite? = null
 
     constructor(inContext: MenuBackground.() -> Unit) : this() {
         inContext()
@@ -43,34 +46,46 @@ class MenuBackground(): ParallaxContainer(), KoinComponent {
     init {
         beatmap.bindTo(beatmapBindable)
 
-        defaultBackground = Sprite("menu/backgrounds/background-1.png") {
-            fillMode = Scaling.Fill
-            size = Vector2f(2560f, 1440f)
-            anchor = Origin.Centre
-        }
-
-        currentBackground = defaultBackground
-
         addChild(
-            BlurredContainer {
+            Container {
                 fillMode = Scaling.Stretch
-                blurAmount = 0.2f
-                anchor = Origin.Custom
-                customAnchor = Vector2f(0.5f, 0.5f)
-                color.w = 0.9f
-                addChild(
-                    BackgroundWrapper().also { wrapper = it }
-                )
-            }
+            }.also { wrapper = it },
+            Triangles {
+                fillMode = Scaling.Stretch
+                minSize = 0.05f
+                maxSize = 0.32f
+                spawnRate = 0.5f
+                baseVelocity = 0.05f
+                triangleOrder = TriangleOrder.Latest
+            }.also { triangles = it }
         )
 
-        wrapper.addChild(defaultBackground)
-
-        beatmapBindable.addListener { _, newBeatmap, _ ->
+        beatmap.addListener { oldBeatmap, newBeatmap, _ ->
             val beatmap = newBeatmap!!
 
-            if (beatmap.beatmapInfo.version != "Danser Intro") {
-                oldBackground = currentBackground
+            val fileHandle = if (beatmap.beatmapInfo.version != "Danser Intro") {
+                val tmpHandle = FileHandle(
+                    System.getenv("localappdata") + "/osu!/Songs/" + beatmap.beatmapSet.directory + File.separator + beatmap.beatmapMetadata.backgroundFile,
+                    FileType.Absolute
+                )
+                if (tmpHandle.file.exists()) tmpHandle else FileHandle(
+                    "assets/textures/menu/backgrounds/background-1.png",
+                    FileType.Classpath
+                )
+            } else FileHandle("assets/textures/menu/backgrounds/background-1.png", FileType.Classpath)
+
+            oldBackground = currentBackground
+
+            pixmap = Pixmap(fileHandle)
+
+            currentBackground = Sprite {
+                fillMode = Scaling.Fill
+                anchor = Origin.Centre
+            }
+
+            wrapper.addChild(currentBackground!!)
+
+            if (oldBeatmap != null) {
                 oldBackground!!.addTransform(
                     Transform(
                         TransformType.Fade,
@@ -82,54 +97,35 @@ class MenuBackground(): ParallaxContainer(), KoinComponent {
                 )
                 oldBackground!!.drawForever = false
 
-                val handle = FileHandle(System.getenv("localappdata") + "/osu!/Songs/" + beatmap.beatmapSet.directory + File.separator + beatmap.beatmapMetadata.backgroundFile, FileType.Absolute)
-
-                if (handle.file.exists()) {
-                    fileTextureHandle = handle
-                    currentBackground = Sprite {
-                        fillMode = Scaling.Fill
-                        anchor = Origin.Centre
-                    }
-                    currentBackground!!.addTransform(
-                        Transform(
-                            TransformType.Fade,
-                            clock.currentTime,
-                            clock.currentTime + 500f,
-                            0f,
-                            1f
-                        )
+                currentBackground!!.addTransform(
+                    Transform(
+                        TransformType.Fade,
+                        clock.currentTime,
+                        clock.currentTime + 500f,
+                        0f,
+                        1f
                     )
-                    wrapper.addChild(currentBackground!!)
-                } else {
-                    if (oldBackground === defaultBackground) {
-                        oldBackground!!.transforms.clear()
-                        oldBackground!!.drawForever = true
-                    } else {
-                        currentBackground = defaultBackground
-                        currentBackground!!.drawForever = true
-                        currentBackground!!.addTransform(
-                            Transform(
-                                TransformType.Fade,
-                                clock.currentTime,
-                                clock.currentTime + 500f,
-                                0f,
-                                1f
-                            )
-                        )
-                        wrapper.addChild(currentBackground!!)
-                    }
-                }
+                )
             }
         }
-
     }
 
     override fun draw(batch: SpriteBatch) {
-        if (currentBackground!!.texture == null && fileTextureHandle != null) {
-            currentBackground!!.texture = Texture(fileTextureHandle!!).region
-            currentBackground!!.size = Vector2f(currentBackground!!.texture!!.getWidth(), currentBackground!!.texture!!.getHeight())
+        if (currentBackground!!.texture == null && pixmap != null) {
+            updateColors(pixmap!!)
+
+            currentBackground!!.texture = Texture(pixmap!!).region
+            currentBackground!!.size =
+                Vector2f(currentBackground!!.texture!!.getWidth(), currentBackground!!.texture!!.getHeight())
         }
+
         super.draw(batch)
+    }
+
+    private fun updateColors(pixmap: Pixmap) {
+        triangles.colorArray = Array(50) {
+            pixmap.pixelAt(random.nextInt(pixmap.width), random.nextInt(pixmap.height))
+        }
     }
 
 }
