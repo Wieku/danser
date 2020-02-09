b9e5135830766dcf9987546fa1d88b9e3dc49b78
Wieku <mail@wieku.me>
2020-02-09 21:43:37 +0100

    Add BlendEquation

---
+++ framework/src/main/kotlin/me/wieku/framework/graphics/blend/BlendEquation.kt
@@ -0,0 +1,15 @@
+package me.wieku.framework.graphics.blend
+
+import me.wieku.framework.utils.EnumWithId
+import org.lwjgl.opengl.GL33.*
+
+/**
+ * Documentation about these factors can be found [here](https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glBlendEquation.xhtml)
+ */
+enum class BlendEquation(override val enumId: Int): EnumWithId {
+    Add(GL_FUNC_ADD),
+    Subtract(GL_FUNC_SUBTRACT),
+    ReverseSubtract(GL_FUNC_REVERSE_SUBTRACT),
+    Min(GL_MIN),
+    Max(GL_MAX)
+}
