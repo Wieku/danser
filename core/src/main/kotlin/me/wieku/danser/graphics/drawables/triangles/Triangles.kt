3672e576b700ae8ba2561e46220ddb1ef5abc795
Wieku <mail@wieku.me>
2020-02-10 14:28:56 +0100

    Optimize Triangles memory usage

--- core/src/main/kotlin/me/wieku/danser/graphics/drawables/triangles/Triangles.kt
+++ core/src/main/kotlin/me/wieku/danser/graphics/drawables/triangles/Triangles.kt
@@ -3,97 +3,45 @@ package me.wieku.danser.graphics.drawables.triangles
 import me.wieku.danser.beatmap.Beatmap
 import me.wieku.framework.animation.Glider
 import me.wieku.framework.di.bindable.Bindable
-import me.wieku.framework.graphics.drawables.containers.Container
+import me.wieku.framework.graphics.drawables.Drawable
 import me.wieku.framework.graphics.drawables.sprite.Sprite
-import me.wieku.framework.math.Origin
+import me.wieku.framework.graphics.drawables.sprite.SpriteBatch
 import me.wieku.framework.math.Scaling
 import me.wieku.framework.math.color.Color
 import org.joml.Vector2f
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.math.*
 
-class Triangles() : Container(), KoinComponent {
-
-    private class Triangle(position: Vector2f, size: Float) : Sprite("misc/triangle.png") {
-        private val helper = Color()
-
-        private val colorIndex = random.nextInt(Int.MAX_VALUE)
-        private val colorShade = random.nextFloat()
-
-        private val oldColor = Color(Float.NaN)
-        private val newColor = Color(Float.NaN)
-
-        private val colorGlider = Glider(1f)
-
-        var useShadow: Boolean = false
-        set(value) {
-            if (value == field) return
-
-            textureName = "misc/triangle" + (if (value) "-shadow" else "") + ".png"
-            field = value
-        }
-
-        init {
-            customSize = true
-            fillMode = Scaling.Fit
-            scale = Vector2f(size)
-
-            anchor = Origin.Custom
-            customAnchor = position
-
-            flipX = random.nextBoolean()
-        }
-
-        override fun update() {
-            colorGlider.update(clock.currentTime)
-
-            color.set(
-                when (colorGlider.value) {
-                    0f -> oldColor
-                    1f -> newColor
-                    else -> Color.mix(oldColor, newColor, colorGlider.value, helper)
-                }
-            )
-
-            super.update()
-        }
-
-        fun updateColors(dark: Color, light: Color) {
-            colorTo(Color.mix(dark, light, colorShade, helper))
-        }
-
-        fun updateColors(colorArray: Array<Color>) {
-            colorTo(colorArray[colorIndex % colorArray.size])
-        }
-
-        private fun colorTo(newColor: Color) {
-            oldColor.set(color)
-
-            if (!this.newColor.x.isNaN()) {
-                colorGlider.reset()
-                colorGlider.addEvent(clock.currentTime, clock.currentTime + 500, 0f, 1f)
-            }
-
-            this.newColor.set(newColor)
-        }
-    }
+class Triangles() : Drawable(), KoinComponent {
 
     private val beatmapBindable: Bindable<Beatmap?> by inject()
 
-    private val separation = 1.4f
     private val bars = 40
     private val triangleSpawnRate = 0.25
 
+    private val triangles = ArrayList<Triangle>()
+    private val trianglesToRemove = ArrayList<Triangle>()
+
+    private val tempSprite = TriangleSprite()
+
     var drawShadows: Boolean = false
+        set(value) {
+            if (value == field) return
+
+            tempSprite.useShadow = value
+
+            field = value
+        }
 
     var colorDark = Color(0f, 1f)
         set(value) {
             if (value == colorDark) return
 
-            children.forEach {
-                (it as Triangle).updateColors(value, colorLight)
+            triangles.forEach {
+                it.updateColors(value, colorLight)
             }
 
             field = value
@@ -103,8 +51,8 @@ class Triangles() : Container(), KoinComponent {
         set(value) {
             if (value == colorLight) return
 
-            children.forEach {
-                (it as Triangle).updateColors(colorDark, value)
+            triangles.forEach {
+                it.updateColors(colorDark, value)
             }
 
             field = value
@@ -113,8 +61,8 @@ class Triangles() : Container(), KoinComponent {
     var colorArray: Array<Color>? = null
         set(value) {
             value?.let {
-                children.forEach {
-                    (it as Triangle).updateColors(value)
+                triangles.forEach {
+                    it.updateColors(value)
                 }
             }
 
@@ -128,6 +76,7 @@ class Triangles() : Container(), KoinComponent {
     var speedMultiplier = 1f
     private var velocity = 0f
     var reactive = true
+    var separation = 1.4f
 
     var spawnRate = 1f
     var trianglesMinimum = 0
@@ -137,23 +86,25 @@ class Triangles() : Container(), KoinComponent {
     var triangleOrder = TriangleOrder.Random
     var triangleDirection = TriangleDirection.Up
 
+    private var triangleSize = Vector2f(0.0f)
+
     constructor(inContext: Triangles.() -> Unit) : this() {
         inContext()
     }
 
     private fun addTriangles(onscreen: Boolean = false) {
-        val maxTriangles = max(trianglesMinimum, (sqrt(drawSize.x * drawSize.y) * triangleSpawnRate * spawnRate).toInt())
+        val maxTriangles =
+            max(trianglesMinimum, (sqrt(drawSize.x * drawSize.y) * triangleSpawnRate * spawnRate).toInt())
 
-        for (i in 0 until maxTriangles - children.size) {
+        for (i in 0 until maxTriangles - triangles.size) {
             addTriangle(onscreen)
         }
 
         if (triangleOrder == TriangleOrder.SmallestToBiggest) {
-            children.sortByDescending { it.scale.y }
+            triangles.sortByDescending { it.size }
         } else if (triangleOrder == TriangleOrder.BiggestToSmallest) {
-            children.sortBy { it.scale.y }
+            triangles.sortBy { it.size }
         }
-
     }
 
     fun addTriangle(onscreen: Boolean) {
@@ -168,26 +119,25 @@ class Triangles() : Container(), KoinComponent {
 
         val triangle = Triangle(position, size)
 
-        if (triangleDirection.directionX != 0.0f) triangle.rotation = PI.toFloat() / 2
-
         if (colorArray != null) {
             triangle.updateColors(colorArray!!)
         } else triangle.updateColors(colorDark, colorLight)
 
         if (triangleOrder == TriangleOrder.Random) {
-            insertChild(
-                triangle,
-                if (children.size > 0) random.nextInt(children.size) else 0
+            triangles.add(
+                if (triangles.size > 0) random.nextInt(triangles.size) else 0,
+                triangle
             )
         } else {
-            addChild(triangle)
+            triangles.add(triangle)
         }
     }
 
     override fun update() {
+        super.update()
 
         if (spawnEnabled)
-            addTriangles(children.size == 0 && startOnScreen)
+            addTriangles(startOnScreen && triangles.isEmpty())
 
         var boost = 0f
 
@@ -203,29 +153,118 @@ class Triangles() : Container(), KoinComponent {
             }
         }
 
-        velocity = max(velocity, min(boost * 0.15f, 0.6f) * speedMultiplier)
+        velocity = max(velocity, min(boost * 0.0725f, 0.3f) * speedMultiplier)
 
         velocity *= 1.0f - 0.05f * clock.time.frameTime / 16.66667f
 
         velocity = max(velocity, baseVelocity)
 
-        children.forEach {
-            (it as Triangle).useShadow = drawShadows
-            val base =
-                clock.time.frameTime / 1000f * velocity * (0.2f + (1.0f - it.scale.y / maxSize * 0.8f) * separation)
+        trianglesToRemove.clear()
 
-            it.customAnchor.add(
+        triangles.forEach {
+            val base =
+                clock.time.frameTime / 1000f * velocity * (1f + separation * (maxSize - it.size) / (maxSize - minSize))
+
+            it.position.add(
                 triangleDirection.directionX * base,
                 triangleDirection.directionY * base
             )
 
-            if(it.customAnchor.y !in -it.scale.y / 2..1f + it.scale.y / 2 || it.customAnchor.x !in -it.scale.x / 2..1f + it.scale.x / 2) {
-                it.drawForever = false
+            it.update(clock.currentTime)
+
+            if (it.position.x !in -it.size / 2..1f + it.size / 2 || it.position.y !in -it.size / 2..1f + it.size / 2) {
+                trianglesToRemove.add(it)
             }
         }
 
-        super.invalidate()
-        super.update()
+        if (triangles.isNotEmpty()) {
+            triangles.removeAll(trianglesToRemove)
+        }
+    }
+
+    override fun updateDrawable() {
+        super.updateDrawable()
+        triangleSize.set(Scaling.Fit.apply(1.0f, 1.0f, drawSize.x, drawSize.y))
+    }
+
+    private var drawArray = emptyArray<Triangle?>()
+
+    override fun draw(batch: SpriteBatch) {
+        drawArray = triangles.toArray(drawArray)
+
+        tempSprite.rotation = if (triangleDirection.directionX != 0.0f) PI.toFloat() / 2 else 0.0f
+
+        for (it in drawArray) {
+            if (it == null) break
+
+            tempSprite.drawSize.set(triangleSize).mul(it.size)
+            tempSprite.drawOrigin.set(tempSprite.drawSize).mul(0.5f)
+            tempSprite.drawPosition.set(drawSize).mul(it.position).sub(tempSprite.drawOrigin).add(drawPosition)
+            tempSprite.drawColor.set(drawColor).mul(it.color)
+            tempSprite.flipX = it.flipX
+            tempSprite.draw(batch)
+        }
+    }
+
+    override fun dispose() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private class TriangleSprite : Sprite("misc/triangle.png") {
+        var useShadow: Boolean = false
+            set(value) {
+                if (value == field) return
+
+                textureName = "misc/triangle" + (if (value) "-shadow" else "") + ".png"
+                field = value
+            }
+    }
+
+    private class Triangle(val position: Vector2f, val size: Float) {
+        private val helper = Color()
+
+        private val colorIndex = random.nextInt(Int.MAX_VALUE)
+        private val colorShade = random.nextFloat()
+        val flipX = random.nextBoolean()
+
+        private val oldColor = Color(Float.NaN)
+        private val newColor = Color(Float.NaN)
+        val color = Color(Float.NaN)
+
+        private var currentTime: Float = 0f
+        private val colorGlider = Glider(1f)
+
+        fun update(time: Float) {
+            currentTime = time
+            colorGlider.update(currentTime)
+
+            color.set(
+                when (colorGlider.value) {
+                    0f -> oldColor
+                    1f -> newColor
+                    else -> Color.mix(oldColor, newColor, colorGlider.value, helper)
+                }
+            )
+        }
+
+        fun updateColors(dark: Color, light: Color) {
+            colorTo(Color.mix(dark, light, colorShade, helper))
+        }
+
+        fun updateColors(colorArray: Array<Color>) {
+            colorTo(colorArray[colorIndex % colorArray.size])
+        }
+
+        private fun colorTo(newColor: Color) {
+            oldColor.set(color)
+
+            if (!this.newColor.x.isNaN()) {
+                colorGlider.reset()
+                colorGlider.addEvent(currentTime, currentTime + 500, 0f, 1f)
+            }
+
+            this.newColor.set(newColor)
+        }
     }
 
     private companion object {
