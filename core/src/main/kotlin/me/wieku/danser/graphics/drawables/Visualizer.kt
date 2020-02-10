57d868e32d3ed0437755c0be64c63038cd901ce0
Wieku <mail@wieku.me>
2020-02-10 15:16:58 +0100

    Improve Visualizer performance

--- core/src/main/kotlin/me/wieku/danser/graphics/drawables/Visualizer.kt
+++ core/src/main/kotlin/me/wieku/danser/graphics/drawables/Visualizer.kt
@@ -5,35 +5,33 @@ import me.wieku.framework.di.bindable.Bindable
 import me.wieku.framework.graphics.drawables.Drawable
 import me.wieku.framework.graphics.drawables.sprite.Sprite
 import me.wieku.framework.graphics.drawables.sprite.SpriteBatch
-import me.wieku.framework.graphics.textures.Texture
-import me.wieku.framework.math.Origin
-import me.wieku.framework.math.rot
-import me.wieku.framework.math.vector2fRad
 import org.joml.Vector2f
 import org.koin.core.KoinComponent
 import org.koin.core.inject
-import kotlin.math.min
+import kotlin.math.cos
+import kotlin.math.sin
 
 class Visualizer() : Drawable(), KoinComponent {
 
-    constructor(inContext: Visualizer.() -> Unit) : this() {
-        inContext()
-    }
-
     private val beatmapBindable: Bindable<Beatmap?> by inject()
 
-    private var deltaSum = 0f
-
+    private val updateDelay = 50f
+    private val rounds = 5
     private val bars = 200
     private val baseDecay = 0.0024f
     private val jumpSize = 5
 
+    private val tempSprite: Sprite = Sprite("pixel")
+    private val amplitudes = FloatArray(bars)
+
     private var jumpCounter = 0
-    private var amplitudes = FloatArray(bars)
+    private var deltaSum = 0f
 
     var barScale = 1f
 
-    private val tempSprite: Sprite = Sprite("pixel")
+    constructor(inContext: Visualizer.() -> Unit) : this() {
+        inContext()
+    }
 
     override fun update() {
         super.update()
@@ -42,7 +40,7 @@ class Visualizer() : Drawable(), KoinComponent {
 
         var decay = clock.time.frameTime * baseDecay
 
-        if (deltaSum >= 50f) {
+        if (deltaSum >= updateDelay) {
 
             beatmapBindable.value?.let {
 
@@ -54,7 +52,7 @@ class Visualizer() : Drawable(), KoinComponent {
                     val fft = it.getTrack().fftData
 
                     for (i in 0 until bars) {
-                        val value = fft[(i + jumpCounter) % bars] * if (timingPoint.kiai) 1f else 0.5f
+                        val value = fft[(jumpCounter + i) % bars] * if (timingPoint.kiai) 1.0f else 0.5f
                         if (value > amplitudes[i]) {
                             amplitudes[i] = value
                         }
@@ -64,7 +62,7 @@ class Visualizer() : Drawable(), KoinComponent {
 
             decay = 0f
             jumpCounter = (jumpCounter + jumpSize) % bars
-            deltaSum -= 50f
+            deltaSum -= updateDelay
         }
 
         for (i in 0 until bars) {
@@ -80,22 +78,24 @@ class Visualizer() : Drawable(), KoinComponent {
     private val tempPos = Vector2f(0f)
 
     override fun draw(batch: SpriteBatch) {
-        tempPos.set(drawPosition).add(drawOrigin)
-
         val barWidth = Math.PI.toFloat() * drawSize.y / bars.toFloat()
-        val cutoff = drawSize.y * 1.5f * barScale
+        val barLength = drawSize.y * 1.5f * barScale
+        val cutoff = 1 / barLength
 
-        for (i in 0 until 5) {
+        tempPos.set(drawPosition).add(drawOrigin).sub(0f, barWidth / 2)
+
+        tempSprite.drawColor.a = 0.3f * drawColor.a
+        tempSprite.drawOrigin.set(0f, barWidth / 2)
+
+        for (i in 0 until rounds) {
             amplitudes.forEachIndexed { j, v ->
-                if (v < 1 / cutoff) return@forEachIndexed
+                if (v < cutoff) return@forEachIndexed
 
-                val rotation = (i / 5.0f + j / bars.toFloat()) * 2 * Math.PI.toFloat()
+                val rotation = (i / rounds.toFloat() + j / bars.toFloat()) * 2 * Math.PI.toFloat()
 
-                tempSprite.drawPosition.set(drawSize.y / 2, 0f).rot(rotation).add(tempPos)
-                tempSprite.drawOrigin.set(0f, 0.5f)
+                tempSprite.drawPosition.set(tempPos.x + cos(rotation) * drawSize.y / 2, tempPos.y + sin(rotation) * drawSize.y / 2)
                 tempSprite.rotation = rotation
-                tempSprite.drawSize.set(v * drawSize.y * 1.5f * barScale, barWidth)
-                tempSprite.drawColor.w = 0.3f * drawColor.w
+                tempSprite.drawSize.set(v * barLength, barWidth)
 
                 tempSprite.draw(batch)
             }
