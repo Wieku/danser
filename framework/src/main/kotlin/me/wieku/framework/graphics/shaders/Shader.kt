5fd027f6ecf05048260f2c5fd3bcf6fe09acdb52
Wieku <mail@wieku.me>
2020-02-11 18:15:38 +0100

    Add memory efficient uniform setters

--- framework/src/main/kotlin/me/wieku/framework/graphics/shaders/Shader.kt
+++ framework/src/main/kotlin/me/wieku/framework/graphics/shaders/Shader.kt
@@ -3,10 +3,11 @@ package me.wieku.framework.graphics.shaders
 import me.wieku.framework.graphics.buffers.VertexAttribute
 import me.wieku.framework.graphics.buffers.VertexAttributeType
 import me.wieku.framework.resource.FileHandle
+import org.joml.*
 import org.lwjgl.opengl.GL33.*
+import org.lwjgl.system.MemoryUtil
 import java.nio.FloatBuffer
 import java.util.*
-import kotlin.collections.HashMap
 
 class Shader(vertex: String, fragment: String) {
     var shaderId: Int = 0
@@ -78,6 +79,107 @@ class Shader(vertex: String, fragment: String) {
         ShaderHelper.removeProgram(shaderId)
     }
 
+    private fun getUniform(name: String, expectedType: VertexAttributeType): VertexAttribute {
+        val uniform = uniforms[name] ?: throw IllegalArgumentException("Uniform does not exist")
+
+        require(uniform.attributeType == expectedType) {
+            "Wrong attribute type. Required :${uniform.attributeType}, given: $expectedType"
+        }
+
+        return uniform
+    }
+
+    fun setUniform1i(name: String, value: Int) {
+        check(isBound) {
+            "Shader is not bound"
+        }
+
+        val uniform = getUniform(name, VertexAttributeType.GlInt)
+        glUniform1i(uniform.location, value)
+    }
+
+    fun setUniform1f(name: String, value: Float) {
+        check(isBound) {
+            "Shader is not bound"
+        }
+
+        val uniform = getUniform(name, VertexAttributeType.GlFloat)
+        glUniform1f(uniform.location, value)
+    }
+
+    //region Vector uniforms
+
+    fun setUniform2f(name: String, value: Vector2f) {
+        check(isBound) {
+            "Shader is not bound"
+        }
+
+        val uniform = getUniform(name, VertexAttributeType.Vec2)
+        glUniform2f(uniform.location, value.x, value.y)
+    }
+
+    fun setUniform3f(name: String, value: Vector3f) {
+        check(isBound) {
+            "Shader is not bound"
+        }
+
+        val uniform = getUniform(name, VertexAttributeType.Vec3)
+        glUniform3f(uniform.location, value.x, value.y, value.z)
+    }
+
+    fun setUniform4f(name: String, value: Vector4f) {
+        check(isBound) {
+            "Shader is not bound"
+        }
+
+        val uniform = getUniform(name, VertexAttributeType.Vec4)
+        glUniform4f(uniform.location, value.x, value.y, value.z, value.w)
+    }
+
+    //endregion
+
+    //region Matrix uniforms
+
+    fun setUniformMatrix3(name: String, value: Matrix3f, transposed: Boolean = false) {
+        check(isBound) {
+            "Shader is not bound"
+        }
+
+        val uniform = getUniform(name, VertexAttributeType.Mat3)
+        glUniformMatrix3fv(uniform.location, transposed, value.get(helperBuffer))
+    }
+
+    fun setUniformMatrix3x2(name: String, value: Matrix3x2f, transposed: Boolean = false) {
+        check(isBound) {
+            "Shader is not bound"
+        }
+
+        val uniform = getUniform(name, VertexAttributeType.Mat32)
+        glUniformMatrix3x2fv(uniform.location, transposed, value.get(helperBuffer))
+    }
+
+    fun setUniformMatrix4(name: String, value: Matrix4f, transposed: Boolean = false) {
+        check(isBound) {
+            "Shader is not bound"
+        }
+
+        val uniform = getUniform(name, VertexAttributeType.Mat4)
+        glUniformMatrix4fv(uniform.location, transposed, value.get(helperBuffer))
+    }
+
+    fun setUniformMatrix4x3(name: String, value: Matrix4x3f, transposed: Boolean = false) {
+        check(isBound) {
+            "Shader is not bound"
+        }
+
+        val uniform = getUniform(name, VertexAttributeType.Mat43)
+        glUniformMatrix4x3fv(uniform.location, transposed, value.get(helperBuffer))
+    }
+
+    //endregion
+
+    //region Universal uniform setters
+
     fun setUniform(name: String, vararg values: Float) {
         check(isBound) {
             "Shader is not bound"
@@ -136,8 +238,15 @@ class Shader(vertex: String, fragment: String) {
         }
     }
 
+    //endregion
+
     private companion object {
         private var stack = ArrayDeque<Int>()
+        private val helperBuffer = MemoryUtil.memAllocFloat(16)
+        get() {
+            field.clear()
+            return field
+        }
     }
 
 }
